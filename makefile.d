1c3044c56cb58890730407ba39675fcf9342d8d1
Niki Roo <niki@nikiroo.be>
2024-06-27 16:59:43 +0200

    makefiles

--- makefile.d
+++ makefile.d
@@ -2,11 +2,13 @@
 # Note: gnu99 (instead of c99) required for libcutils-net
 
 NAME   = cutils
+NAMES  = $(NAME) check net
 srcdir = cutils
 dstdir = ../bin
 
 CFLAGS   += -Wall -pedantic -I./ -std=gnu99
 CXXFLAGS += -Wall -pedantic -I./
+# LDFLAGS  += -lcheck
 PREFIX   =  /usr/local
 
 ifdef DEBUG
@@ -14,23 +16,32 @@ CFLAGS   += -ggdb -O0
 CXXFLAGS += -ggdb -O0
 endif
 
-.PHONY: all install uninstall clean mrpropre mrpropre \
-	utils check net
-
-all: utils check net
-
-utils: $(dstdir)/libcutils.o
-
-check: $(dstdir)/libcutils-check.o
-
-net: $(dstdir)/libcutils-net.o
+.PHONY: all build install uninstall clean mrpropre mrpropre \
+	$(NAMES)
 
 SOURCES=$(wildcard $(srcdir)/*.c)
 HEADERS=$(wildcard $(srcdir)/*.h)
 OBJECTS=$(SOURCES:%.c=%.o)
 
+# Main targets
+
+all: build
+
+build: $(NAMES)
+
+cutils: $(dstdir)/libcutils.o
+check: $(dstdir)/libcutils-check.o
+net: $(dstdir)/libcutils-net.o
+
+################
+# Dependencies #
+################
+# OBJECTS+=$(dstdir)/libcutils.o
+# $(dstdir)/libcutils.o:
+# 	$(MAKE) --no-print-directory -C cutils/ cutils
 $(srcdir)/array.o:   $(srcdir)/array.[ch]
 $(srcdir)/desktop.o: $(srcdir)/desktop.[ch] $(srcdir)/array.h
+################
 
 $(dstdir)/libcutils.o: $(OBJECTS)
 	mkdir -p $(dstdir)
@@ -51,7 +62,7 @@ debug:
 	$(MAKE) -f $(srcdir)/makefile.d DEBUG=1
 
 clean:
-	rm -f $(srcdir)/*.o $(srcdir)/check/*.o $(srcdir)/net/*.o
+	rm -f $(srcdir)/*.o $(srcdir)/*/*.o
 
 mrproper: mrpropre
 
@@ -62,6 +73,7 @@ mrpropre: clean
 	rmdir $(dstdir) 2>/dev/null || true
 
 install: 
+	mkdir -p "$(PREFIX)/lib"
 	cp $(dstdir)/libcutils.o       "$(PREFIX)/lib/libcutils.a"
 	cp $(dstdir)/libcutils-check.o "$(PREFIX)/lib/libcutils-check.a"
 	cp $(dstdir)/libcutils-net.o   "$(PREFIX)/lib/libcutils-net.a"
@@ -75,10 +87,12 @@ uninstall:
 	rm "$(PREFIX)/lib/libcutils.a"
 	rm "$(PREFIX)/lib/libcutils-check.a"
 	rm "$(PREFIX)/lib/libcutils-net.a"
+	rmdir "$(PREFIX)/lib/"                    2>/dev/null
 	rm "$(PREFIX)/include/$(srcdir)/net/"*.h
 	rm "$(PREFIX)/include/$(srcdir)/check/"*.h
 	rm "$(PREFIX)/include/$(srcdir)/"*.h
 	rmdir "$(PREFIX)/include/$(srcdir)/net"   2>/dev/null
 	rmdir "$(PREFIX)/include/$(srcdir)/check" 2>/dev/null
 	rmdir "$(PREFIX)/include/$(srcdir)"       2>/dev/null
+	rmdir "$(PREFIX)/include/"                2>/dev/null
 
