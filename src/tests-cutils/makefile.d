2aa1098a12a605a3a7d6624e4b4be87e1d302ac4
Niki Roo <niki@nikiroo.be>
2024-06-30 13:35:48 +0200

    makefile

--- src/tests-cutils/makefile.d
+++ src/tests-cutils/makefile.d
@@ -3,7 +3,10 @@
 NAME   = tests-cutils
 NAMES  = $(NAME)
 srcdir = $(NAME)
-dstdir = ../bin
+
+ifeq ($(dstdir),)
+dstdir = $(srcdir)/bin
+endif
 
 CFLAGS   += -Wall -pedantic -I./ -std=c99
 CXXFLAGS += -Wall -pedantic -I./
@@ -29,13 +32,23 @@ build: $(NAMES)
 
 $(NAME): deps $(dstdir)/$(NAME)
 
+# Test program, so running = running tests
+run: run-test
+test: build
+run-test: test
+	@echo
+	$(dstdir)/$(NAME)
+run-test-more: test
+	@echo
+	$(dstdir)/$(NAME) --more
+
 ################
 # Dependencies #
 ################
 ## Libs (if needed)
 deps:
-	$(MAKE) --no-print-directory -C cutils/ cutils
-	$(MAKE) --no-print-directory -C cutils/ check
+	$(MAKE) dstdir=$(dstdir) --no-print-directory -C cutils/ cutils
+	$(MAKE) dstdir=$(dstdir) --no-print-directory -C cutils/ check
 DEPS=$(dstdir)/libcutils.o $(dstdir)/libcutils-check.o
 
 ## Headers
@@ -49,22 +62,16 @@ $(dstdir)/$(NAME): $(DEPS) $(OBJECTS)
 	mkdir -p $(dstdir)
 	$(CC) $(CFLAGS) $(LDFLAGS) $(DEPS) $(OBJECTS) -o $@
 
-# Test program, so running = running tests
-run: run-test
-test: build
-
-run-test: test
-	@echo
-	$(dstdir)/$(NAME)
-run-test-more: test
-	@echo
-	$(dstdir)/$(NAME) --more
-
 clean:
-	rm -f $(OBJECTS) $(DEPENDS)
+	rm -f $(OBJECTS) 
+	rm -f $(DEPENDS)
+	rm -f $(DEPS)
 
 mrproper: mrpropre
 mrpropre: clean
 	rm -f $(dstdir)/$(NAME)
 	rmdir $(dstdir) 2>/dev/null || true
+
+install uninstall:
+	@echo Those are tests, nothing to install/uninstall
 
